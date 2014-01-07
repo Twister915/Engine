a635ba58c3d9c20157341506ae976d29fde7ae3f
Jake <jake0oo0andminecraft@gmail.com>
2014-01-07 01:22:18 -0600

    debug punishments

--- GearzBungee/src/main/java/net/tbnr/gearz/player/bungee/GearzPlayer.java
+++ GearzBungee/src/main/java/net/tbnr/gearz/player/bungee/GearzPlayer.java
@@ -134,11 +134,10 @@ public class GearzPlayer {
         Object bansl = dbObject.get("punishments");
         if (bansl == null || !(bansl instanceof BasicDBList)) {
             ProxyServer.getInstance().getLogger().info("fml2214");
-
             if (bansl == null) {
                 ProxyServer.getInstance().getLogger().info("fml");
-
             }
+
             bansl = new BasicDBList();
         }
         BasicDBList bans = (BasicDBList) bansl;
