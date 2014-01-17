d6cb2c1025f0dc26e1e1014d22cda248bdbb97cc
Igor Randjelovic <rigor789@gmail.com>
2014-01-17 18:39:47 +0100

    Work In Progress. Basically it will run our servers once it's done.

---
+++ GearzBootstrap/src/main/java/net/tbnr/CommandExecutor.java
@@ -0,0 +1,10 @@
+package net.tbnr;
+
+/**
+ * Created by rigor789 on 2014.01.17..
+ */
+public abstract class CommandExecutor {
+
+    public abstract boolean onCommand(Command cmd, String[] args);
+
+}
