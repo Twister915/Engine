ef0883049728284c6822c5fed22a4428322a7927
georgePadolsey <georgepadolsey@gmail.com>
2014-01-23 18:24:22 +0000

    Changed unit tester
    and image to chat bukkit util

---
+++ GearzEngine/src/main/java/net/tbnr/gearz/unittesting/UnitTesterConfig.java
@@ -0,0 +1,18 @@
+package net.tbnr.gearz.unittesting;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by George on 20/01/14.
+ * <p/>
+ * Purpose Of File:
+ * <p/>
+ * Latest Change:
+ */
+public class UnitTesterConfig {
+
+    @Getter
+    @Setter
+    public String title;
+}
