f0acffbac204755782f1716b975ed4f98dcdff47
Jake <jake0oo0andminecraft@gmail.com>
2014-01-27 20:46:41 -0600

    Fix clearchat

--- GearzEngine/src/main/java/net/tbnr/gearz/Gearz.java
+++ GearzEngine/src/main/java/net/tbnr/gearz/Gearz.java
@@ -9,6 +9,7 @@ import net.tbnr.gearz.effects.EnchantmentEffect;
 import net.tbnr.gearz.effects.EnderBar;
 import net.tbnr.gearz.game.single.GameManagerSingleGame;
 import net.tbnr.gearz.netcommand.NetCommand;
+import net.tbnr.gearz.player.ClearChat;
 import net.tbnr.gearz.player.GearzNickname;
 import net.tbnr.gearz.player.GearzPlayerUtils;
 import net.tbnr.gearz.server.Server;
@@ -125,6 +126,7 @@ public final class Gearz extends TPlugin implements TCommandHandler, TDatabaseMa
         registerEvents(new PlayerListener());
         registerEvents(new ColoredTablist());
         registerEvents(new EnderBar.EnderBarListeners());
+        registerCommands(new ClearChat());
         new TabListener();
         EnchantmentEffect.addEnchantmentListener();
         this.inventoryRefresher = new InventoryRefresher();
