d42cfe3f4971d42b0c50262f090e1263b627cd5e
Joey Sacchini <joey.sacchini264@gmail.com>
2014-01-28 14:26:10 -0500

    NPE because you can't pass null values to this.

--- GearzEngine/src/main/java/net/tbnr/gearz/player/GearzPlayer.java
+++ GearzEngine/src/main/java/net/tbnr/gearz/player/GearzPlayer.java
@@ -209,6 +209,7 @@ public final class GearzPlayer {
     }
 
     public static GearzPlayer playerFromPlayer(Player player) {
+        if (player == null) throw new NullPointerException("Cannot pass a null player!");
         return GearzPlayer.playerFromTPlayer(Gearz.getInstance().getPlayerManager().getPlayer(player));
     }
 
