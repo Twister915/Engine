d83f1d36cdcaa03340754b9018b13760e8273665
georgePadolsey <georgepadolsey@gmail.com>
2014-02-07 22:42:13 +0000

    Added move 2 gearz and utility method

---
+++ GearzEngine/src/main/java/net/tbnr/util/Move2Gearz.java
@@ -0,0 +1,12 @@
+package net.tbnr.util;
+
+/**
+ * Represents A class that needs to be merged into gearz
+ * Created by George on 07/02/14.
+ * <p/>
+ * Purpose Of File: Represents A class that needs to be merged into gearz
+ * <p/>
+ * Latest Change: Added it
+ */
+public interface Move2Gearz {
+}
