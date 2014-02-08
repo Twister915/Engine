d8abb511ba180bb86a55cf13e1aa2a4ce16e233c
Jake <jake0oo0andminecraft@gmail.com>
2014-02-08 14:39:26 -0600

    Copy defaults

--- GearzBukkitPermissions/src/main/java/net/cogz/permissions/bukkit/GearzBukkitPermissions.java
+++ GearzBukkitPermissions/src/main/java/net/cogz/permissions/bukkit/GearzBukkitPermissions.java
@@ -21,6 +21,7 @@ public final class GearzBukkitPermissions extends JavaPlugin {
     public void onEnable() {
         GearzBukkitPermissions.instance = this;
         this.permsManager = new PermissionsManager();
+        getConfig().options().copyDefaults(true);
         saveConfig();
         getServer().getPluginManager().registerEvents(this.permsManager, this);
         PermissionsCommands permsCommands = new PermissionsCommands();
