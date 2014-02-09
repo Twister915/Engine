e2864fd8474f8667559f107b5d005194d5a28d33
georgePadolsey <georgepadolsey@gmail.com>
2014-02-09 15:23:03 +0000

    Add project licensing

--- GearzBungee/src/main/java/net/tbnr/gearz/command/NetCommandHandler.java
+++ GearzBungee/src/main/java/net/tbnr/gearz/command/NetCommandHandler.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2014.
+ * Cogz Development LLC USA
+ * All Right reserved
+ *
+ * This software is the confidential and proprietary information of Cogz Development, LLC.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with Cogz LLC.
+ */
+
 package net.tbnr.gearz.command;
 
 import java.lang.annotation.ElementType;
