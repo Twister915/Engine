4f2c518db3248f83e9e8b5a88485ada9e865cc71
LBJDaKing23 <lockerzmodding@gmail.com>
2014-02-13 05:10:23 -0500

    Added RefreshPlayer method needs implementing into teleport spots. Should fix invisible players bug

--- GearzEngine/src/main/java/net/tbnr/util/RandomUtils.java
+++ GearzEngine/src/main/java/net/tbnr/util/RandomUtils.java
@@ -1,6 +1,8 @@
 package net.tbnr.util;
 
+import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.injector.BukkitUnwrapper;
+import com.comphenix.protocol.reflect.FieldAccessException;
 import com.comphenix.protocol.reflect.accessors.Accessors;
 import net.tbnr.gearz.Gearz;
 import org.bukkit.Location;
@@ -9,6 +11,7 @@ import org.bukkit.entity.Player;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.math.BigInteger;
+import java.util.List;
 
 public class RandomUtils implements GUtility {
 
@@ -58,6 +61,32 @@ public class RandomUtils implements GUtility {
             width.set(entityPlayer, collision ? 0.6F : 0.0F);
             length.set(entityPlayer, collision ? 1.8F : 0.0F);
         } catch (IllegalAccessException ignored) {
+        }
+    }
+
+    /**
+     * Refreshes multiple players for a list of players.
+     *
+     * @param players1 - the players that needs updating aka the invisible players.
+     * @param players2 - the list of players getting the update.
+     */
+    public void refreshPlayers(List<Player> players1, List<Player> players2) {
+        for(Player player : players1) {
+            refreshPlayer(player, players2);
+        }
+    }
+
+    /**
+     * Refreshes the player for the list of players.
+     * Hopefully will fix invisible player issues. Based of Protocollib dev this should work.
+     *
+     * @param player - the player that needs updating aka the invisible player.
+     * @param players - the list of players getting the update.
+     */
+    public void refreshPlayer(Player player, List<Player> players) {
+        try {
+            ProtocolLibrary.getProtocolManager().updateEntity(player, players);
+        } catch (FieldAccessException ignored) {
         }
     }
 }
