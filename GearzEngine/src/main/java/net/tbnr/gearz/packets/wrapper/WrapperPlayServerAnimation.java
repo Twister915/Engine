640ee1388c5ee7985936fb829d0df8125dc0384b
georgePadolsey <georgepadolsey@gmail.com>
2014-03-15 00:56:23 +0000

    changed donate to get

---
+++ GearzEngine/src/main/java/net/tbnr/gearz/packets/wrapper/WrapperPlayServerAnimation.java
@@ -0,0 +1,15 @@
+package net.tbnr.gearz.packets.wrapper;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by George on 14/03/14.
+ * <p/>
+ * Purpose Of File:
+ * <p/>
+ * Latest Change:
+ */
+public class WrapperPlayServerAnimation {
+	private static final boolean debug = false; //TODO use gearz debug mode
+	private static final Logger log = null; //TODO get logger
+}
---
+++ GearzEngine/src/main/java/net/tbnr/util/RedFactory.java
@@ -0,0 +1,15 @@
+package net.tbnr.util;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by George on 14/03/14.
+ * <p/>
+ * Purpose Of File:
+ * <p/>
+ * Latest Change:
+ */
+public class RedFactory {
+	private static final boolean debug = false; //TODO use gearz debug mode
+	private static final Logger log = null; //TODO get logger
+}
