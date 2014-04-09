379e747c92b485ad12af5d1f7a47645a6e8ce42a
Jake <jake0oo0andminecraft@gmail.com>
2014-04-09 16:02:17 -0500

    UUID Debug

--- GearzBungeePunishments/src/main/java/net/cogz/punishments/bungee/PunishmentCommands.java
+++ GearzBungeePunishments/src/main/java/net/cogz/punishments/bungee/PunishmentCommands.java
@@ -91,7 +91,9 @@ public class PunishmentCommands implements TCommandHandler {
                 return TCommandStatus.SUCCESSFUL;
             }
 
-            manager.punishPlayer(gearzTarget.getUuid(), sender.getName(), reason, PunishmentType.PERMANENT_BAN, null);
+            System.out.println(gearzTarget.getUuid());
+            System.out.println(gearzTarget.getProxiedPlayer().getUniqueId().toString());
+            manager.punishPlayer(gearzTarget.getProxiedPlayer().getUniqueId().toString(), sender.getName(), reason, PunishmentType.PERMANENT_BAN, null);
 
             sender.sendMessage(GearzBungeePunishments.getInstance().getFormat("banned-player", false, true, new String[]{"<reason>", reason}, new String[]{"<target>", gearzTarget.getUsername()}));
             if (gearzTarget.getProxiedPlayer() == null) return TCommandStatus.SUCCESSFUL;
