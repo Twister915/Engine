b9cefefdfe47aaf2f326f1133920de0d16f6ef92
Jake <jake0oo0andminecraft@gmail.com>
2014-05-16 23:59:31 -0500

    remove

--- GearzHub/src/main/java/net/cogz/engine/hub/modules/Spawn.java
+++ GearzHub/src/main/java/net/cogz/engine/hub/modules/Spawn.java
@@ -134,7 +134,6 @@ public class Spawn implements Listener, TCommandHandler {
 
     public void setSpawn(Location location) {
         GearzHub.getInstance().getConfig().set("spawn", TPlugin.encodeLocationString(location));
-        GearzHub.getInstance().saveConfig();
         updateSpawn();
     }
 
